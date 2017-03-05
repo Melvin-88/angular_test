1143408f28b14aa4c41811123df094bc1d6501ba
Melvin-88 <taisen747@gmail.com>
2017-03-05 17:16:41 +0200

    'add'

---
+++ app/js/app.js
@@ -0,0 +1,173 @@
+'use strict';
+
+var app = angular.module('app', [
+    'ui.bootstrap',
+    'ngAnimate',
+    'infinite-scroll',
+    'ngFileUpload'
+]);
+
+app.directive('userImg', function(){
+    return{
+        restrict: 'E',
+        templateUrl: '../template/images.html'
+    }
+});
+
+app.directive('modal', function(){
+    return{
+        restrict: 'E',
+        templateUrl: '../template/modal.html'
+    }
+});
+
+app.controller('UserController', UserImages);
+
+UserImages.$inject = ['$scope', '$log', '$http','Images','Upload'];
+function UserImages ($scope, $log, $http, Images, Upload){
+    var that  = this;
+    var images  = new Images();
+    that.newFile = {
+        file: null,
+        image_url: null
+    };
+
+    that.images   = images;
+
+    that.search   = function (query) {
+        console.log(query.length)
+        if(query.length>=3){
+            images.search(query);
+            that.images = images;
+        }
+        if(query.length){
+            images.search(query);
+            that.images = images;
+        }
+    };
+
+    that.addFavorite = function (id) {
+        var list;
+        if(localStorage.favorite){
+            list = JSON.parse(localStorage.favorite);
+        }else{
+            list = []
+        }
+        var newArr = list.filter((e)=>{
+            return e == id;
+        });
+        if(newArr.length>0){
+            return false
+        }else{
+            list.push(id);
+        }
+        localStorage.setItem('favorite', JSON.stringify(list));
+    };
+
+    that.open = function (id) {
+        images.getOneById(id, function (url) {
+            $('.img-modal').attr('src', url);
+            $('#myModal').modal('show');
+        }.bind(this));
+    };
+
+    that.uploadFiles = function () {
+        if ((!that.newFile.file && !that.newFile.source_image_url)) return;
+        var url = 'http://upload.giphy.com/v1/gifs';
+        var data = {
+            username: 'Melvin',
+            api_key: 'dc6zaTOxFJmzC',
+            file: that.newFile.file,
+            source_image_url: that.newFile.image_url,
+            tags: ''
+        };
+
+        return Upload.upload({
+            url: url,
+            data: data
+        }).then(function(response){
+            $('.add_img').text('Your image added...');
+            if(response){
+                var id = response.data.data.id;
+                that.addFavorite(id);
+            }
+        });
+    };
+}
+
+app.factory('Images', function($http, $log) {
+    var Images = function () {
+        this.apiKey = 'api_key=dc6zaTOxFJmzC';
+        this.items = [];
+        this.offset = 0;
+        this.limit = 30;
+        this.busy = false;
+        this.action = 'trending';
+        this.queryString = '';
+        this.itemData = {};
+    }
+
+    Images.prototype.makeUrl = function () {
+        var url = 'http://api.giphy.com/v1/gifs/';
+        var apiKeyStr = this.apiKey;
+
+        switch (this.action) {
+            case 'search':
+                url = url + this.action + '?' + apiKeyStr + '&q=' + this.queryString;
+                break;
+            default:
+                url = url + this.action + '?' + apiKeyStr
+        }
+
+        return url + "&limit=" + this.limit + "&offset=" + this.offset;
+    }
+
+    Images.prototype.search = function (query) {
+        if (!query) {
+            this.items = [];
+            this.offset = 0;
+            this.action = 'trending';
+            this.nextPage();
+        }
+        this.items = [];
+        this.offset = 0;
+        this.action = 'search';
+        this.queryString = query;
+        this.nextPage();
+    }
+
+    Images.prototype.nextPage = function() {
+        var that = this;
+
+        if (this.busy) return;
+        this.busy = true;
+
+        var url = this.makeUrl();
+        $log.debug('url:' + url);
+        $http.get(url).then(function(response) {
+            var items = response.data.data;
+            for (var i = 0; i < items.length; i++) {
+                this.items.push(items[i]);
+                this.offset++;
+            }
+        }.bind(this));
+        setTimeout(function(){
+            that.busy = false;
+        }, 100);
+    };
+
+    Images.prototype.getOneById =  function (id, cb) {
+        $http.get('http://api.giphy.com/v1/gifs/'+ id + '?' + this.apiKey).then(function(response) {
+            cb(response.data.data.images.original.url);
+        }.bind(this));
+        return this;
+    }
+    return Images;
+})
+
+
+
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+
+
+
+//css
+gulp.task('css', function() {
+    return gulp.src('app/css/*.css')
+        .pipe(concatCss("main.css"))
+        .pipe(minifyCss())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('app/src/css'));
+});
+
+//js
+gulp.task('js', function() {
+    return gulp.src([
+        'app/js/jquery-3.1.1.min.js',
+        'app/bower_components/angular/angular.js',
+        'app/bower_components/ng-file-upload/ng-file-upload.min.js',
+        'app/js/ng-infinite-scroll.js',
+        'app/js/bootstrap.min.js',
+        'app/bower_components/angular-bootstrap/ui-bootstrap.min.js',
+        'app/bower_components/angular-animate/angular-animate.min.js',
+        'app/js/app.js'
+    ])
+        .pipe(concat("main.js"))
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('app/src/js'));
+});
+
